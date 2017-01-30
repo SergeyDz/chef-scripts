3a2741875e74f1d17ac3b45d6dc42312228ee681
SergeyDz <dzyuban@gmail.com>
2017-01-30 11:36:33 +0000

    Separated vagrant stop-remove-start roles

--- marathon.cluster.recreate.sh
+++ marathon.cluster.recreate.sh
@@ -1,10 +1,11 @@
 knife ssh 'name:sdzyuban-mesos-*' -x vagrant -P vagrant 'rm /etc/chef/client.pem'
 yes | knife node bulk delete 'sdzyuban-mesos(.*)' && yes | knife client bulk delete 'sdzyuban-mesos(.*)' 
 
-knife bootstrap 10.1.1.231 --ssh-user vagrant --ssh-password 'vagrant' --sudo --use-sudo-password --node-name sdzyuban-mesos-01 --run-list 'role[zookeeper], role[mesos-master], role[mesos-slave], role[marathon], role[mesosphere]' 
-knife bootstrap 10.1.1.232 --ssh-user vagrant --ssh-password 'vagrant' --sudo --use-sudo-password --node-name sdzyuban-mesos-02 --run-list 'role[zookeeper], role[mesos-master], role[mesos-slave], role[marathon], role[mesosphere]'
-knife bootstrap 10.1.1.233 --ssh-user vagrant --ssh-password 'vagrant' --sudo --use-sudo-password --node-name sdzyuban-mesos-03 --run-list 'role[zookeeper], role[mesos-master], role[mesos-slave], role[marathon], role[mesosphere]' 
-knife bootstrap 10.1.1.234 --ssh-user vagrant --ssh-password 'vagrant' --sudo --use-sudo-password --node-name sdzyuban-mesos-04 --run-list 'role[mesos-slave], role[mesosphere]' 
-knife bootstrap 10.1.1.235 --ssh-user vagrant --ssh-password 'vagrant' --sudo --use-sudo-password --node-name sdzyuban-mesos-05 --run-list 'role[mesos-slave], role[mesosphere]' 
-knife bootstrap 10.1.1.236 --ssh-user vagrant --ssh-password 'vagrant' --sudo --use-sudo-password --node-name sdzyuban-mesos-06 --run-list 'role[mesos-slave], role[mesosphere]' 
+#until ping -c1 10.1.1.231 &>/dev/null; do :; done
+knife bootstrap 10.1.1.231 --ssh-user vagrant --ssh-password 'vagrant' --sudo --use-sudo-password --node-name sdzyuban-mesos-01 --run-list 'role[zookeeper], role[mesos-master], role[mesos-slave], role[marathon], role[mesosphere], role[docker]'
+knife bootstrap 10.1.1.232 --ssh-user vagrant --ssh-password 'vagrant' --sudo --use-sudo-password --node-name sdzyuban-mesos-02 --run-list 'role[zookeeper], role[mesos-master], role[mesos-slave], role[marathon], role[mesosphere], role[docker]'  &/
+knife bootstrap 10.1.1.233 --ssh-user vagrant --ssh-password 'vagrant' --sudo --use-sudo-password --node-name sdzyuban-mesos-03 --run-list 'role[zookeeper], role[mesos-master], role[mesos-slave], role[marathon], role[mesosphere], role[docker]'  &/
+knife bootstrap 10.1.1.234 --ssh-user vagrant --ssh-password 'vagrant' --sudo --use-sudo-password --node-name sdzyuban-mesos-04 --run-list 'role[mesos-slave], role[mesosphere], role[docker]'  &/
+knife bootstrap 10.1.1.235 --ssh-user vagrant --ssh-password 'vagrant' --sudo --use-sudo-password --node-name sdzyuban-mesos-05 --run-list 'role[mesos-slave], role[mesosphere], role[docker]'  &/
+knife bootstrap 10.1.1.236 --ssh-user vagrant --ssh-password 'vagrant' --sudo --use-sudo-password --node-name sdzyuban-mesos-06 --run-list 'role[mesos-slave], role[mesosphere], role[docker]'
 
--- windows.cluster.recreate.sh
+++ windows.cluster.recreate.sh
@@ -3,8 +3,8 @@
 # delete all nodes 
 yes | knife node bulk delete '(.*)-pc' && yes | knife client bulk delete '(.*)-pc'
 
-knife winrm "0483-pc.fg.local" "powershell.exe Remove-Item c:\\chef\\client.pem -Force -Recurse" -m -x fg\\sdzyuban -P $1  &\
-knife winrm "sdzyuban-pc.fg.local" "powershell.exe Remove-Item c:\\chef\\client.pem -Force -Recurse" -m -x fg\\sdzyuban -P $1
+knife winrm "0483-pc.fg.local" "powershell.exe Remove-Item c:\\chef\\* -Force -Recurse -ErrorAction SilentlyContinue" -m -x fg\\sdzyuban -P $1  
+knife winrm "sdzyuban-pc.fg.local" "powershell.exe Remove-Item c:\\chef\\* -Force -Recurse -ErrorAction SilentlyContinue" -m -x fg\\sdzyuban -P $1
 
 
 # create unsecured roles
@@ -20,9 +20,23 @@ knife data bag from file passwords chef_keys.json
 
 
 # add node roles
-knife node run_list add 0483-pc 'role[winrm]' 
-knife node run_list add sdzyuban-pc 'role[winrm]' 
+knife node run_list add 0483-pc 'role[winrm],role[vagrant-remove]'   
+knife node run_list add sdzyuban-pc 'role[winrm],role[vagrant-remove]' 
 
 # run the winrm roles 
 knife winrm "0483-pc" "chef-client -c c:/chef/client.rb" -m -x fg\\sdzyuban -P $1  &\
 knife winrm "sdzyuban-pc" "chef-client -c c:/chef/client.rb" -m -x fg\\sdzyuban -P $1
+
+
+# add node roles
+knife node run_list add 0483-pc 'role[vagrant-create]'
+knife node run_list add sdzyuban-pc 'role[vagrant-create]'
+
+# run the winrm roles
+knife winrm "0483-pc" "chef-client -c c:/chef/client.rb" -m -x fg\\sdzyuban -P $1  &\
+knife winrm "sdzyuban-pc" "chef-client -c c:/chef/client.rb" -m -x fg\\sdzyuban -P $1
+
+
+knife winrm "0483-pc" "ping 10.1.1.234" -m -x fg\\sdzyuban -P $1
+knife winrm "sdzyuban-pc" "ping 10.1.1.231" -m -x fg\\sdzyuban -P $1
+#knife winrm "0483-pc" "ping 10.1.1.236" -m -x fg\\sdzyuban -P $1
