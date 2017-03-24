c318cb8740d056a12ec68f038e7ae86934a1e61b
SergeyDz <dzyuban@gmail.com>
2017-03-24 13:28:28 +0000

    backup

--- windows.cluster.recreate.sh
+++ windows.cluster.recreate.sh
@@ -3,12 +3,12 @@
 # delete all nodes 
 yes | knife node bulk delete '(.*)-pc' && yes | knife client bulk delete '(.*)-pc'
 
-knife winrm "0483-pc.fg.local" "powershell.exe Remove-Item c:\\chef\\* -Force -Recurse -ErrorAction SilentlyContinue" -m -x fg\\sdzyuban -P $1  
+#knife winrm "0483-pc.fg.local" "powershell.exe Remove-Item c:\\chef\\* -Force -Recurse -ErrorAction SilentlyContinue" -m -x fg\\sdzyuban -P $1  
 knife winrm "sdzyuban-pc.fg.local" "powershell.exe Remove-Item c:\\chef\\* -Force -Recurse -ErrorAction SilentlyContinue" -m -x fg\\sdzyuban -P $1
 
 
 # create unsecured roles
-knife bootstrap windows winrm 0483-pc -N 0483-pc -x fg\\sdzyuban -P $1 --run-list 'role[hostfile]' &\
+#knife bootstrap windows winrm 0483-pc -N 0483-pc -x fg\\sdzyuban -P $1 --run-list 'role[hostfile]' &\
 knife bootstrap windows winrm sdzyuban-pc -N sdzyuban-pc -x fg\\sdzyuban -P $1 --run-list 'role[hostfile]' --json-attributes '{"vagrant": { "local" : { "path" : "D:/Storage/VirtualBox/Marathon"} }}'
 
 knife vault refresh passwords chef
@@ -20,23 +20,23 @@ knife data bag from file passwords chef_keys.json
 
 
 # add node roles
-knife node run_list add 0483-pc 'role[winrm],role[vagrant-remove]'   
+#knife node run_list add 0483-pc 'role[winrm],role[vagrant-remove]'   
 knife node run_list add sdzyuban-pc 'role[winrm],role[vagrant-remove]' 
 
 # run the winrm roles 
-knife winrm "0483-pc" "chef-client -c c:/chef/client.rb" -m -x fg\\sdzyuban -P $1  &\
+#knife winrm "0483-pc" "chef-client -c c:/chef/client.rb" -m -x fg\\sdzyuban -P $1  &\
 knife winrm "sdzyuban-pc" "chef-client -c c:/chef/client.rb" -m -x fg\\sdzyuban -P $1
 
 
 # add node roles
-knife node run_list add 0483-pc 'role[vagrant-create]'
+#knife node run_list add 0483-pc 'role[vagrant-create]'
 knife node run_list add sdzyuban-pc 'role[vagrant-create]'
 
 # run the winrm roles
-knife winrm "0483-pc" "chef-client -c c:/chef/client.rb" -m -x fg\\sdzyuban -P $1  &\
+#knife winrm "0483-pc" "chef-client -c c:/chef/client.rb" -m -x fg\\sdzyuban -P $1  &\
 knife winrm "sdzyuban-pc" "chef-client -c c:/chef/client.rb" -m -x fg\\sdzyuban -P $1
 
 
-knife winrm "0483-pc" "ping 10.1.1.234" -m -x fg\\sdzyuban -P $1
+#knife winrm "0483-pc" "ping 10.1.1.234" -m -x fg\\sdzyuban -P $1
 knife winrm "sdzyuban-pc" "ping 10.1.1.231" -m -x fg\\sdzyuban -P $1
 #knife winrm "0483-pc" "ping 10.1.1.236" -m -x fg\\sdzyuban -P $1
